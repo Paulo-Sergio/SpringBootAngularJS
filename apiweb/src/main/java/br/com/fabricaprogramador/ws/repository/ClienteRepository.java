eef3d96eaee72daf7d6b79059923e79c49506477
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-07-06 22:23:34 -0300

    incluindo dependencias necessaria para a conexão e realização do CRUD com JPA na api web

--- apiweb/src/main/java/br/com/fabricaprogramador/ws/repository/ClienteRepository.java
+++ apiweb/src/main/java/br/com/fabricaprogramador/ws/repository/ClienteRepository.java
@@ -1,9 +1,12 @@
 package br.com.fabricaprogramador.ws.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.fabricaprogramador.ws.model.Cliente;
+
 @Repository
-public interface ClienteRepository extends JPARepository<Cliente, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
 	
 }
