29bac9d4c267d2655f40aadec3c0e4ed78b71374
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-07-10 22:01:01 -0300

    separando controllers do angular para trabalhar com rotas

--- apiweb/src/main/resources/static/js/app.js
+++ apiweb/src/main/resources/static/js/app.js
@@ -1,2 +1,18 @@
 // criando modulo principal
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+	  
+	$routeProvider
+		.when('/clientes', {
+			templateUrl: 'view/cliente.html',
+			controller: 'clienteController'
+		})
+		
+		.otherwise({
+			rediretTo: '/'
+		});
+
+	  // configure html5 to get links working on jsfiddle
+	  $locationProvider.html5Mode(true);
+});
